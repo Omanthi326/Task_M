395a8437cc953d2e474beb5aa2660bc792f7851c
Omanthika Bandara <163089471+Omanthi326@users.noreply.github.com>
2025-04-02 03:52:09 +0530

    Update Login.jsx

--- client/src/components/Login.jsx
+++ client/src/components/Login.jsx
@@ -16,7 +16,7 @@ function Login({ isAuthenticated, setIsAuthenticated }) {
     
     try {
       const res = await axios.post(
-        "http://localhost:4000/api/v1/user/login",
+        "http://13.48.137.48:4000/api/v1/user/login",
         { email, password },
         {
           withCredentials: true,
